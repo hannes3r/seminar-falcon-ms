8a2cb407d375be9831da070985f1a0d560cd43ec
hannes3r <hannes.dreyer@outlook.de>
2021-05-12 10:21:26 +0200

    added entity visualization and changed route names

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ import falcon
 
 # import custom routes for different modules
 from src.tokenization import Tokenization 
-from src.visualization import Visualization
 from src.lemmatization import Lemmatization
+from src.entities import EntityRecognition
+
+from src.visualization import Dependency_Visualization, EntityRecognition_Visualization
 
 # init app
 app = falcon.App()
 
 # add modules to app
-app.add_route('/token/list', Tokenization())
-app.add_route('/token/visualization', Visualization())
+app.add_route('/token', Tokenization())
 app.add_route('/lemma', Lemmatization())
+app.add_route('/entities', EntityRecognition())
+
+app.add_route('/visualization/dependencies', Dependency_Visualization())
+app.add_route('/visualization/entities', EntityRecognition_Visualization())
--- src/visualization.py
+++ src/visualization.py
@@ -4,12 +4,7 @@ from spacy import displacy
 
 nlp = spacy.load('de_core_news_sm')
 
-def visualizeDependencies(s):
-    doc = nlp(s)
-    svg = displacy.render(doc, style="dep")
-    return svg
-
-class Visualization:
+class Dependency_Visualization:
     def on_get(self, req, resp):
         text = "";
 
@@ -18,7 +13,32 @@ class Visualization:
                 text = str(value)
 
         if text!="": 
-            svg_data = visualizeDependencies(text)
+            doc = nlp(text)
             resp.status = falcon.HTTP_200
             resp.content_type = falcon.MEDIA_HTML
-            resp.text = svg_data
+            resp.text = displacy.render(doc, style="dep")
+
+class EntityRecognition_Visualization:
+    def on_get(self, req, resp):
+        text = "";
+
+        for key, value in req.params.items():
+            if key=="text":
+                text = str(value)
+
+        if text != "":
+            doc = nlp(text)
+            response = []
+            for ent in doc.ents:
+                response.append({
+                    "text": ent.text,
+                    "label": ent.label_,
+                    "start_position": ent.start_char,
+                    "end_position": ent.end_chart
+                })
+            resp.status = falcon.HTTP_200
+            resp.text = json.dumps(response)
+        else:
+            response = {"error":"Error while converting, please check your input"}
+            resp.status = falcon.HTTP_200
+            resp.text = json.dumps(response)
